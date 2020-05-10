1e8c3b4f5911754588b85aee16e7c0b30c53c9b1
Matthew Apuya <m.apuya19@gmail.com>
2020-05-10 04:59:06 -0500

    added Weather class

---
+++ Bonus/src/Weather.java
@@ -0,0 +1,49 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Weather {
+    public static void main(String[] args) {
+        System.out.print("Enter a ZipCode: ");
+        String zipCode = new Scanner(System.in).next();
+        String useURL = "http://www.rssweather.com/zipcode/" + zipCode + "/rss.php";
+
+        try {
+            URL url = new URL(useURL);
+            Scanner input = new Scanner(url.openStream());
+
+            while (input.hasNext()) {
+                if (input.nextLine().contains("<item>")) {
+                    String read = input.nextLine();
+                    // System.out.println(read);
+                    String title = read.substring(read.indexOf("<title>") + 7, read.indexOf("</title>"));
+                    if (title.contains("Little")) {
+                        System.out.println();
+                    }
+                    else {
+                        System.out.println(title);
+                    }
+
+                    input.nextLine();
+
+                    if (input.nextLine().contains("<link>")) {
+                        String read2 = input.nextLine();
+                        String desc = read2.substring(read2.indexOf("<description>") + 13, read2.indexOf("</description>"));
+                        System.out.println(desc);
+                        System.out.println();
+                    }
+                }
+            }
+        }
+
+        catch (MalformedURLException e) {
+            System.out.println("Invalid URL!");
+        }
+
+        catch (java.io.IOException e) {
+            System.out.println("IO Errors");
+        }
+    }
+}
