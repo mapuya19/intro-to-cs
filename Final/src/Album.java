cdf3a28ce9a70149c25e3ab9d097c2bae6860583
Matthwe Apuya <m.apuya19@gmail.com>
2020-05-14 16:00:47 -0500

    added Final module

---
+++ Final/src/Album.java
@@ -0,0 +1,9 @@
+public class Album {
+    private String author;
+    private String songName;
+
+    public Album(String author, String songName) {
+        this.author = author;
+        this.songName = songName;
+    }
+}
---
+++ Final/src/SongCollection.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class SongCollection {
+    private ArrayList<Album> collection;
+
+    public SongCollection() {
+        collection =  new ArrayList<Album>();
+    }
+
+    public void add(String songName, String author) {
+
+    }
+
+    public void sort() {
+
+    }
+}
